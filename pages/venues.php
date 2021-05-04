79c2840ad0247f4a140261fdbba6f9c829d70158
Patrick Ikoi <patrick@softcom.ng>
2021-05-04 11:01:31 +0100

    updated venues.php

--- pages/venues.php
+++ pages/venues.php
@@ -7,76 +7,32 @@
         exit('Please fill both the name and address of the venue!');
     }
 
+    $param_name = trim($_POST["name"]);
+    $param_address = trim($_POST["address"]);
+
     // Processing form data when form is submitted
     if($_SERVER["REQUEST_METHOD"] == "POST"){
     
-        // Validate username
-        if(empty(trim($_POST["email"]))){
-            $email_err = "Please enter a email.";
-        } else{
-            // Prepare a select statement
-            $sql = "SELECT id FROM users WHERE email = ?";
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE email = ?";
             
-            if($stmt = mysqli_prepare($link, $sql)){
-                // Bind variables to the prepared statement as parameters
-                mysqli_stmt_bind_param($stmt, "s", $param_email);
-                
-                // Set parameters
-                $param_email = trim($_POST["email"]);
-                
-                // Attempt to execute the prepared statement
-                if(mysqli_stmt_execute($stmt)){
-                    /* store result */
-                    mysqli_stmt_store_result($stmt);
-                    
-                    if(mysqli_stmt_num_rows($stmt) == 1){
-                        $email_err = "This email is already taken.";
-                    } else{
-                        $email = trim($_POST["email"]);
-                    }
-                } else{
-                    echo "Oops! Something went wrong. Please try again later.";
-                }
-
-                // Close statement
-                mysqli_stmt_close($stmt);
-            }
-        }
-        
-        // Validate password
-        if(empty(trim($_POST["password"]))){
-            $password_err = "Please enter a password.";     
-        } elseif(strlen(trim($_POST["password"])) < 6){
-            $password_err = "Password must have atleast 6 characters.";
-        } else{
-            $password = trim($_POST["password"]);
-        }
-        
         // Check input errors before inserting in database
-        if(empty($email_err) && empty($password_err)){
+        // Prepare an insert statement
+        $sql = "INSERT INTO venues (name, address) VALUES (?, ?)";  
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ss", $param_name, $param_address);
             
-            // Prepare an insert statement
-            $sql = "INSERT INTO users (email, password) VALUES (?, ?)";
-            
-            if($stmt = mysqli_prepare($link, $sql)){
-                // Bind variables to the prepared statement as parameters
-                mysqli_stmt_bind_param($stmt, "ss", $param_email, $param_password);
-                
-                // Set parameters
-                $param_email = $username;
-                $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-                
-                // Attempt to execute the prepared statement
-                if(mysqli_stmt_execute($stmt)){
-                    // Redirect to login page
-                    header("location: index.php");
-                } else{
-                    echo "Oops! Something went wrong. Please try again later.";
-                }
-
-                // Close statement
-                mysqli_stmt_close($stmt);
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to login page
+                header("location: index.php");
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
             }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
         }
         
         // Close connection
