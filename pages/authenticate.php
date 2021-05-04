ad39ee23cd4ecde12dd4222038c39cd866fd27be
Patrick Ikoi <patrick@softcom.ng>
2021-05-04 10:08:45 +0100

    updated db layer

--- pages/authenticate.php
+++ pages/authenticate.php
@@ -1,17 +1,8 @@
 <?php
     session_start();
-    // Change this to your connection info.
-    $DATABASE_HOST = 'localhost';
-    $DATABASE_USER = 'root';
-    $DATABASE_PASS = '';
-    $DATABASE_NAME = 'school_portal';
-    // Try and connect using the info above.
-    $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-    if ( mysqli_connect_errno() ) {
-        // If there is an error with the connection, stop the script and display the error.
-        exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-    }
-
+    
+    require '../db/db.php';
+    
     // Now we check if the data from the login form was submitted, isset() will check if the data exists.
     if ( !isset($_POST['email'], $_POST['password']) ) {
         // Could not get the data that should have been sent.
